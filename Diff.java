f12cc3380b19d8bb2488e72a7c7cfcf38551b7c8
ManjeetShrvstv <manjeetshrvstv@gmail.com>
2021-12-10 19:29:36 +0530

    Diff file added

---
+++ Diff.java
@@ -0,0 +1,9 @@
+public class Diff {
+    public static void main(String args[])
+{
+    int a= 12;
+    int b=6;
+
+    System.out.println("Difference is:" ,a-b );
+}
+}
--- Sum.java
+++ Sum.java
@@ -3,6 +3,6 @@ public class Sum {
     {
         int a=5;
         int b=5;
-        system.out.println("Sum is : " a+b);
+        System.out.println("Sum is : ",  a+b);
     }
 }
